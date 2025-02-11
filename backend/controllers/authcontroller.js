bac01dc32268f03aba84160615203b0e50dfb1fd
yasminebh <bhouriyasmine9@gmail.com>
2025-02-11 12:02:50 +0100

    dash

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -1,3 +1,4 @@
+
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
@@ -7,37 +8,44 @@ const createError = require('../utils/appError');
 // REGISTER USER
 exports.signup = async (req, res, next) => {
   try {
+    // Vérification si l'utilisateur existe déjà
     const user = await User.findOne({ email: req.body.email });
     if (user) {
       return next(new createError('User already exists!', 400));
     }
+
+    // Vérification du rôle
+    const validRoles = ['admin', 'gestionnaire', 'technicien']; // Définir les rôles autorisés
+    if (!validRoles.includes(req.body.role)) {
+      return next(new createError('Invalid role. Role must be one of admin, gestionnaire, or technicien.', 400));
+    }
+
+    // Hachage du mot de passe
     const hashedPassword = await bcrypt.hash(req.body.password, 12);
     const newUser = await User.create({
       ...req.body,
       password: hashedPassword,
     });
 
-    //assign jwt
-    // Assign JWT (json web token) to user
-    const token = jwt.sign({ _id: newUser._id }, 'secretkey123', {
-      expiresIn: '300d',
-    });
-  
+    // Assignation du JWT
+    const token = jwt.sign({ _id: newUser._id }, 'secretkey123', { expiresIn: '300d' });
+
     res.status(201).json({
-        status: 'success',
-        token,
-        message: 'User registered successfully',
-        user: {
-            _id: newUser._id,
-            name: newUser.name,
-            email: newUser.email,
-            role : newUser.role,
-        },
-  });
+      status: 'success',
+      token,
+      message: 'User registered successfully',
+      user: {
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        role: newUser.role,
+      },
+    });
   } catch (error) {
     next(error);
   }
 };
+
 // LOGGING USER
 exports.login = async (req, res, next) => {
     try {
