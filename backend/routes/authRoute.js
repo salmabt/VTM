5a8a2ded224a223fc96f71b66fdab719233840fd
yasminebh <bhouriyasmine9@gmail.com>
2025-03-11 23:31:04 +0100

    email

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const authController = require('../controllers/authcontroller');
+const isAdmin = require('../middleware/isAdmin'); // Importez le middleware
+const authMiddleware = require('../middleware/authMiddleware'); // Middleware d'authentification
 
 const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-router.get('/validate/:userId', authController.validateUser);// Route pour valider l'utilisateur 
-router.post('/validate/:userId', authController.validateUser);
 
-
+// Route de validation (protégée par authMiddleware et isAdmin)
+router.post('/validate/:userId', authMiddleware, isAdmin, authController.validateUser);
 
 module.exports = router;
