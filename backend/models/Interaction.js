a87f64333d8ede5171feabd6400344ebe6de1e93
yasminebh <bhouriyasmine9@gmail.com>
2025-05-07 20:06:35 +0100

    chat

--- backend/models/Interaction.js
+++ backend/models/Interaction.js
@@ -6,16 +6,8 @@ const InteractionSchema = new mongoose.Schema({
   email: String, 
   phone: String,
   address: String,
-  service: String ,
   title_de_livraison: String,
   description: String,
-<<<<<<< HEAD
-  nom_client: String,
-  phone: String,
-  email: String, 
-  address: String,
-=======
->>>>>>> f049af40b3d9963fd90b4b76dadaca9f8048b3a2
   relatedTask: { 
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Task'
